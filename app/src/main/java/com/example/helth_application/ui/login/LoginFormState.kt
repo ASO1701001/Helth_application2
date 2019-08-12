ca8a2eb620680cd5aa46e28e5c772c969f07de30
hiyoko-nagaike <53502567+hiyoko-nagaike@users.noreply.github.com>
2019-08-12 17:29:12 +0900

    test

---
+++ app/src/main/java/com/example/helth_application/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.example.helth_application.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
